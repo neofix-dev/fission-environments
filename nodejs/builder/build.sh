718f46754084636dfcd5695ec4fb3fbb961f96c8
Timo Pulkkinen <timo@kevathanki.com>
2023-06-06 16:50:41 +0300

    Added wrappers for redirecting npm error output to stdout.

--- nodejs/builder/build.sh
+++ nodejs/builder/build.sh
@@ -3,4 +3,4 @@ cd ${SRC_PKG}
 if [[ -n "$NPM_TOKEN" ]] && [[ -n "$NPM_REGISTRY" ]]; then
     npm set //${NPM_REGISTRY}/:_authToken ${NPM_TOKEN}
 fi
-npm install && cp -r ${SRC_PKG} ${DEPLOY_PKG}
+npm-wrapper install && cp -r ${SRC_PKG} ${DEPLOY_PKG}
---
+++ nodejs/npm-wrapper.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+TMPFILE=$(mktemp "npm-wrapper.XXXXXX")
+
+on_exit() {
+  rm -rf "$TMPFILE"
+}
+trap on_exit EXIT
+
+echo Running npm "$@"
+npm "$@" 2>&1 | tee -a "$TMPFILE"
+logfile=$(grep "npm ERR.*\.log" "$TMPFILE" | awk -F' ' '{print $3}')
+if [ -n "$logfile" ]; then
+    cat "$logfile"
+fi
