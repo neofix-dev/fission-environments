501b7ad82ba59b6100a52b1bedcf77c9d3ffc849
Timo Pulkkinen <timo@kevathanki.com>
2023-06-15 12:41:36 +0300

    Install Typescript globally. Use npm build if node_modules already exists.

--- nodejs/builder/build.sh
+++ nodejs/builder/build.sh
@@ -3,4 +3,12 @@ cd ${SRC_PKG}
 if [[ -n "$NPM_TOKEN" ]] && [[ -n "$NPM_REGISTRY" ]]; then
     npm-wrapper set //${NPM_REGISTRY}/:_authToken ${NPM_TOKEN}
 fi
-npm-wrapper install --no-audit --omit-dev --no-fund && cp -r ${SRC_PKG} ${DEPLOY_PKG}
+if [[ -d "node_modules" ]]; then
+    [[ -d "node_modules/.bin" ]] && chmod +x node_modules/.bin/*
+    echo "Executing npm run build..."
+    npm-wrapper run build
+  else
+    echo "Executing npm install..."
+    npm-wrapper install --no-audit --omit-dev --no-fund --no-progress
+fi
+test $? -eq 0 && cp -r ${SRC_PKG} ${DEPLOY_PKG}
