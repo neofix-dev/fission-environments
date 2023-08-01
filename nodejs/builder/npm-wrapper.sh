dcf3e5735b62c88d2f57d41c3feb2b67dd1af89b
Timo Pulkkinen <timo@kevathanki.com>
2023-08-01 14:14:48 +0300

    0.4.3 add build status message

--- nodejs/builder/npm-wrapper.sh
+++ nodejs/builder/npm-wrapper.sh
@@ -1,23 +1,39 @@
 #!/bin/sh
-set -o pipefail
-set -e
-env
 
+env
 TMPFILE=$(mktemp "npm-wrapper.XXXXXX")
 
 on_exit() {
   rm -f "$TMPFILE"
 }
+
+output_log() {
+  printf '[%s]\t%s\t%s\n' "${SRC_PKG}" "${1}" "${2}"
+}
 trap on_exit EXIT
 
-echo Running npm "$@"
+echo Running npm "$@" > "$TMPFILE"
 
-if ! (npm "$@" 2>&1 | tee -a "$TMPFILE"); then
-  echo npm "$@" failed.
-  logfile=$(grep "npm ERR.*\.log" "$TMPFILE" | awk -F' ' '{print $3}')
-  if [ -n "$logfile" ]; then
-    cat "$logfile"
-  fi
-  exit 1
+npm --quiet "$@" > $TMPFILE 2>&1
+retval=$?
+
+logfile=$(grep "npm ERR.*\.log" "$TMPFILE" | awk -F' ' '{print $3}')
+
+while read -r line
+do
+  output_log info "$line"
+done < "${TMPFILE}"
+
+if [ -n "${logfile}" ]; then
+  while read -r line
+  do
+    output_log debug "$line"
+  done < "${logfile}"
 fi
-exit 0
+if [ -"$retval" -ne 0 ]; then
+  output_log error STATUS_BUILD_FAILED
+else
+  output_log info STATUS_BUILD_SUCCESSFUL
+fi
+
+exit "$retval"
